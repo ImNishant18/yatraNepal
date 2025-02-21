acbaac3f2375ff541304e6adf672823727605d66
ImNishant18 <timalsinanishant01@gmail.com>
2025-02-21 14:08:31 +0545

    Edit all

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 import Header from "@/components/Home Page/header";
-import HomeTouristGuide from "@/components/Home Page/tourist_guide";
+// import HomeTouristGuide from "@/components/Home Page/tourist_guide";
 import ImageSlider from "@/components/Home Page/image_slider";
 import HotelBanner from "@/components/Home Page/hotel_banner";
 import MoneyExchange from "@/components/Home Page/moneyexchange";
-import PlaceBanner from "@/components/Home Page/place_banner";
 import PromotionalCard from "@/components/Home Page/promotional-card";
-import MoreExplore from "@/components/Home Page/more-explore";
+import PlaceBanner from "@/components/Home Page/place_banner";
 export default function Home() {
   return (
     <div>
@@ -13,14 +12,13 @@ export default function Home() {
       <div className="pt-[200px] md:pt-[240px] pb-8">
         <ImageSlider />
       </div>
-      <PlaceBanner />
-      <HomeTouristGuide />
-      <div className="bg-gray-100 py-8">
-        <HotelBanner />
-        <MoneyExchange />
-        <PromotionalCard />
-        <MoreExplore />
+
+      <div className="container mx-auto">
+        <PlaceBanner />
       </div>
+      <HotelBanner />
+      <MoneyExchange />
+      <PromotionalCard />
     </div>
   );
 }
